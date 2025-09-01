ed9dc817082eb6eeb62077b81d908214a1ef5adb
Walter Bernardi <walter.bernardi@unibe.ch>
2025-09-01 23:12:28 +0200

    test piu' avanzato

--- test/test_chain.py
+++ test/test_chain.py
@@ -4,8 +4,7 @@ import os
 import pytest
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from mymodule import add
-
+from mymodule import add, divide
 
 @pytest.mark.dependency()
 def test_step1():
@@ -13,8 +12,20 @@ def test_step1():
 
 @pytest.mark.dependency(depends=["test_step1"])
 def test_step2():
-    assert add(2, 2) == 4
+    result = add(2, 3)
+    assert result == 5
 
 @pytest.mark.dependency(depends=["test_step2"])
 def test_step3():
+    result = divide(10, 2)
+    assert result == 5
+
+@pytest.mark.dependency(depends=["test_step3"])
+def test_step4():
+    # divisione per zero gestita
+    assert divide(4, 0) == "Division by zero error"
+
+@pytest.mark.dependency(depends=["test_step4"])
+def test_step5():
+    # piccolo “scherzetto” finale
     assert add(3, 3) == 6
