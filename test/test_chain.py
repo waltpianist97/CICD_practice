357f8ec310c1f5f35ff8c1d91a4a43edd5e82b15
Walter Bernardi <walter.bernardi@unibe.ch>
2025-09-01 23:01:45 +0200

    chain

--- test/test_chain.py
+++ test/test_chain.py
@@ -6,17 +6,15 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from mymodule import add
 
-state = {"last_result": None}
 
+@pytest.mark.dependency()
 def test_step1():
-    state["last_result"] = add(1, 1)
-    assert state["last_result"] == 2
+    assert add(1, 1) == 2
 
+@pytest.mark.dependency(depends=["test_step1"])
 def test_step2():
-    # Dipende dal risultato del test_step1
-    assert state["last_result"] == 2
-    state["last_result"] *= 2
+    assert add(2, 2) == 4
 
+@pytest.mark.dependency(depends=["test_step2"])
 def test_step3():
-    # Dipende da step2
-    assert state["last_result"] == 4
+    assert add(3, 3) == 6
