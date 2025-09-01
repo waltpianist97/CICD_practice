bddde35e25bd99b03ff659cf57cc50c0811a6215
Walter Bernardi <walter.bernardi@unibe.ch>
2025-09-01 22:56:42 +0200

    more tests

--- test/test_mymodule.py
+++ test/test_mymodule.py
@@ -1,8 +1,19 @@
 import sys
 import os
+import pytest
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from mymodule import add
+from mymodule import add, divide, subtract
 
 def test_add():
     assert add(1, 1) == 2
+
+def test_divide():
+    assert divide(4, 2) == 2
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        divide(4, 0)
+
+def test_subtract():
+    assert subtract(4, 2) == 2
